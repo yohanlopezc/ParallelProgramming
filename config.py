d4c1b8d895359ef85ea35cc0eb33aa04d6ebbc71
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2024-06-09 14:26:06 -0500

    Update config.py

--- config.py
+++ config.py
@@ -53,3 +53,77 @@ if_rank_0=If(test=Compare(left=Call(func=Attribute(value=Name(id='hvd',ctx=Load(
         FunctionDef(name='hvd_adapt_epochs',args=arguments(args=[arg(arg='epochs',annotation=None)],vararg=None,kwonlyargs=[],kw_defaults=[],kwarg=None,defaults=[]),body=[Return(value=Call(func=Name(id='max',ctx=Load()),args=[Num(n=1),Call(func=Attribute(value=Name(id='math',ctx=Load()),attr='ceil',ctx=Load()),args=[BinOp(left=Name(id='epochs',ctx=Load()),op=FloorDiv(),right=Call(func=Attribute(value=Name(id='hvd',ctx=Load()),attr='size',ctx=Load()),args=[],keywords=[]))],keywords=[])],keywords=[]))],decorator_list=[],returns=None),
         FunctionDef(name='hvd_adapt_steps',args=arguments(args=[arg(arg='steps',annotation=None)],vararg=None,kwonlyargs=[],kw_defaults=[],kwarg=None,defaults=[]),body=[Return(value=Call(func=Name(id='max',ctx=Load()),args=[Num(n=1),Call(func=Attribute(value=Name(id='math',ctx=Load()),attr='ceil',ctx=Load()),args=[BinOp(left=Name(id='steps',ctx=Load()),op=FloorDiv(),right=Call(func=Attribute(value=Name(id='hvd',ctx=Load()),attr='size',ctx=Load()),args=[],keywords=[]))],keywords=[])],keywords=[]))],decorator_list=[],returns=None)
     ]
+class Torch:
+
+## Torchvision model names:
+    possible_model_names_torchvision = [
+    'resnet18',
+    'alexnet',
+    'vgg16',
+    'squeezenet1_0',
+    'densenet161',
+    'inception_v3',
+    'googlenet',
+    'shufflenet_v2_x1_0',
+    'mobilenet_v2',
+    'resnext50_32x4d',
+    'wide_resnet50_2',
+    'mnasnet1_0'
+    ]
+
+## Torchvision optimizer names:
+    possible_optim_names_torchvision = [
+    	'Adadelta',
+    	'Adagrad',
+    	'Adam',
+    	'Adamax',
+    	'AdamW',
+    	'ASGD',
+    	'LBFGS',
+    	'RMSprop',
+    	'Rprop',
+    	'SGD',
+    	'SparseAdam'
+    ]
+
+## IMPORTS:
+    imports=[
+        Import(names=[alias(name='horovod.torch',asname='hvd')]),
+        Import(names=[alias(name='torch',asname=None)])
+    ]
+
+## HOROVOD CONFIGS
+    configs = [
+        Expr(value=Call(func=Attribute(value=Name(id='hvd',ctx=Load()),attr='init',ctx=Load()),args=[],keywords=[])),
+        If(test=Call(func=Attribute(value=Attribute(value=Name(id='torch',ctx=Load()),attr='cuda',ctx=Load()),attr='is_available',ctx=Load()),args=[],keywords=[]),body=[Expr(value=Call(func=Attribute(value=Attribute(value=Name(id='torch',ctx=Load()),attr='cuda',ctx=Load()),attr='set_device',ctx=Load()),args=[Call(func=Attribute(value=Name(id='hvd',ctx=Load()),attr='local_rank',ctx=Load()),args=[],keywords=[])],keywords=[]))],orelse=[])
+    ]
+
+##Adaptaciones del código
+
+    # adapt data loaders:
+    data_sampler=Assign( targets=[Name(id='hvd_sampler_', ctx=Store())], value=Call( func=Attribute( value=Attribute( value=Attribute( value=Attribute( value=Name( id='torch', ctx=Load()), attr='utils', ctx=Load()), attr='data', ctx=Load()), attr='distributed', ctx=Load()), attr='DistributedSampler', ctx=Load()), args=[], keywords=[ keyword( arg='dataset', value=Name( id='dataset', ctx=Load())), keyword( arg='num_replicas', value=Call( func=Attribute( value=Name( id='hvd', ctx=Load()), attr='size', ctx=Load()), args=[], keywords=[])), keyword( arg='rank', value=Call( func=Attribute( value=Name( id='hvd', ctx=Load()), attr='rank', ctx=Load()), args=[], keywords=[]))]))
+    data_sampler_keyword=keyword(arg='sampler',value=Name(id='hvd_sampler_',ctx=Load()))
+
+
+    model_to_cuda=Expr(value=Call(func=Attribute(value=Name(id='model',ctx=Load()),attr='cuda',ctx=Load()),args=[],keywords=[]))
+    # adapt optimizer
+    adapt_opt=Expr(value=Call(func=Name(id='hvd_adapt_optimizer',ctx=Load()),args=[Name(id='optimizer',ctx=Load()), Name(id='model',ctx=Load())],keywords=[]))
+    # broadcast_parameters
+    broadcast_parameters=Expr(value=Call(func=Attribute(value=Name(id='hvd',ctx=Load()),attr='broadcast_parameters',ctx=Load()),args=[Call(func=Attribute(value=Name(id='model', ctx=Load()),attr='state_dict',ctx=Load()),args=[],keywords=[])],keywords=[keyword(arg='root_rank',value=Num(n=0))]))
+    # adapt loss & accuracy
+    adapt_loss=[
+        AugAssign( target=Name( id='test_loss', ctx=Store()), op=Div(), value=Call( func=Name( id='len', ctx=Load()), args=[Name( id='test_sampler', ctx=Load())], keywords=[])),
+        Assign( targets=[Name( id='test_loss', ctx=Store())], value=Call( func=Name( id='hvd_metric_average', ctx=Load()), args=[ Name( id='test_loss', ctx=Load()), Str(s='avg_loss')], keywords=[]))
+    ]
+    adapt_accuracy=[
+        AugAssign( target=Name( id='test_accuracy', ctx=Store()), op=Div(), value=Call( func=Name( id='len', ctx=Load()), args=[Name( id='test_sampler', ctx=Load())], keywords=[])),
+        Assign( targets=[Name( id='test_accuracy', ctx=Store())], value=Call( func=Name( id='hvd_metric_average', ctx=Load()), args=[ Name( id='test_accuracy', ctx=Load()), Str(s='avg_accuracy')], keywords=[]))
+    ]
+
+    if_rank_0=If(test=Compare(left=Call(func=Attribute(value=Name(id='hvd',ctx=Load()),attr='rank',ctx=Load()),args=[],keywords=[]),ops=[Eq()],comparators=[Num(n=0)]),body=[],orelse=[])
+    ## auxiliar functions
+    aux_funcs = [
+        FunctionDef(name='hvd_adapt_optimizer',args=arguments(args=[arg(arg='optimizer',annotation=None),arg(arg='model',annotation=None)],vararg=None,kwonlyargs=[],kw_defaults=[],kwarg=None,defaults=[]),body=[AugAssign(target=Subscript(value=Subscript(value=Attribute(value=Name(id='optimizer',ctx=Load()),attr='param_groups',ctx=Load()),slice=Index(value=Num(n=0)),ctx=Load()),slice=Index(value=Str(s='lr')),ctx=Store()),op=Mult(),value=Call(func=Attribute(value=Name(id='hvd',ctx=Load()),attr='size',ctx=Load()),args=[],keywords=[])),Expr(value=Call(func=Attribute(value=Name(id='hvd',ctx=Load()),attr='broadcast_optimizer_state',ctx=Load()),args=[Name(id='optimizer',ctx=Load())],keywords=[keyword(arg='root_rank',value=Num(n=0))])),Assign(targets=[Name(id='optimizer',ctx=Store())],value=Call(func=Attribute(value=Name(id='hvd',ctx=Load()),attr='DistributedOptimizer',ctx=Load()),args=[Name(id='optimizer',ctx=Load())],keywords=[keyword(arg='named_parameters',value=Call(func=Attribute(value=Name(id='model',ctx=Load()),attr='named_parameters',ctx=Load()),args=[],keywords=[]))])),Return(value=Name(id='optimizer',ctx=Load()))],decorator_list=[],returns=None),
+        FunctionDef(name='hvd_metric_average',args=arguments(args=[arg(arg='val',annotation=None),arg(arg='name',annotation=None)],vararg=None,kwonlyargs=[],kw_defaults=[],kwarg=None,defaults=[]),body=[Assign(targets=[Name(id='tensor',ctx=Store())],value=Call(func=Attribute(value=Name(id='torch',ctx=Load()),attr='tensor',ctx=Load()),args=[Name(id='val',ctx=Load())],keywords=[])),Assign(targets=[Name(id='avg_tensor',ctx=Store())],value=Call(func=Attribute(value=Name(id='hvd',ctx=Load()),attr='allreduce',ctx=Load()),args=[Name(id='tensor',ctx=Load())],keywords=[keyword(arg='name',value=Name(id='name',ctx=Load()))])),Return(value=Call(func=Attribute(value=Name(id='avg_tensor',ctx=Load()),attr='item',ctx=Load()),args=[],keywords=[]))],decorator_list=[],returns=None)
+    ]
+
