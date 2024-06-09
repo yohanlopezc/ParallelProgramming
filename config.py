adfa948b09e95a972b4f8f1d27683942addcca22
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2024-06-09 14:21:07 -0500

    Update config.py
    clase keras

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
+from ast import *
+
+class Keras:
+
+    ## Keras model names
+    possible_model_names_keras=[
+        'Model', 'Sequential', 'Xception',
+        'VGG16', 'VGG19', 'ResNet50',
+        'ResNet50V2', 'ResNet101', 'ResNet101V2',
+        'ResNet152', 'ResNet152V2', 'InceptionV3',
+        'InceptionResNetV2', 'MobileNet', 'DenseNet121',
+        'DenseNet169', 'DenseNet201', 'NASNetLarge',
+        'NASNetMobile', 'MobileNetV2'
+    ]
 
