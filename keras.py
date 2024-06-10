a8a2eb38cba8ae2d30504a36fe7659f74d16c1c6
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2024-06-09 19:47:39 -0500

    Update keras.py

--- keras.py
+++ keras.py
@@ -10,3 +10,22 @@ import time
 import ntpath
 from copy import deepcopy as copy
 
+class RootNodeException(Exception):
+
+    pass
+
+def classname(cls):
+    return cls.__class__.__name__.lower()
+
+def get_body_nodes(root_node):
+    ret_list = []
+    try:
+        body_node = getattr(root_node, 'body')
+        ret_list.append(body_node)
+        orelse_node = getattr(root_node, 'orelse')
+        ret_list.append(orelse_node)
+        finally_node = getattr(root_node, 'finalbody')
+        ret_list.append(finally_node)
+    except Exception:
+        pass
+    return ret_list
