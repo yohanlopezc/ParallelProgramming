a787c75f762da8bd08b6f411d45dea6bca9a7c3b
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2024-06-09 19:49:48 -0500

    Update keras.py

--- keras.py
+++ keras.py
@@ -52,3 +52,102 @@ def recover_code_from_ast_file(filename, save_to_file):
     code_to_exec = "node = " + parsed_code
     exec(code_to_exec, globals())
     return generate_code_from_ast(node, save_to_file)
+
+ef generate_horovodized_code(root_node, filename):
+
+    directory = os.path.join('PARALLEL', 'KERAS')
+    filename = os.path.join(directory, filename)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    python_code = au.unparse(root_node)
+
+    file = open(filename, 'w')
+    file.write(python_code)
+    file.close()
+    try:
+        import black
+        out = check_output(["black", filename], stderr=subprocess.STDOUT)
+    except ModuleNotFoundError:
+        print("Could not format code! Run pip install black")
+    return filename
+
+def add_import(root_node, code):
+    name = code.names[0].name
+    last_idx = 0
+    exists = False
+    hvd = [code]
+    body = get_body_nodes(root_node)[0]
+    for idx, elem in enumerate(body):
+        if classname(elem).startswith('import'):
+            last_idx = idx
+            if elem.names[0].name == name:
+                if VERBOSE:
+                    print(name + ' import already exists')
+                exists = True
+        else:
+            if not exists:
+                last_idx = add_code(body, idx, hvd)
+            break
+
+    return last_idx
+
+def add_horovod_initialization(framework, root_node, idx):
+
+    body = get_body_nodes(root_node)[0]
+    if framework == 'tf1':
+        return add_code(body, idx, copy(hvdconfig.configs_tf1))
+    else:
+        return add_code(body, idx, copy(hvdconfig.configs_tf2))
+
+def gen_aux_fn(root_node, idx, code):
+    body = get_body_nodes(root_node)[0]
+    hvd = [code]
+    return add_code(body, idx, hvd)
+def find_wrappers(root_node):
+
+    def find_wrappers_recursive(body, possible_names):
+        for elem in body:
+            try:
+                if classname(elem) == 'classdef':
+                    inheritances = elem.bases
+                    for inh in inheritances:
+                        if inh.id in possible_names:
+                            possible_names.append(elem.name)
+                            if VERBOSE:
+                                print(f"The class {elem.name} inherits from {inh.id}!")
+                else:
+                    for body2 in get_body_nodes(elem):
+                        find_model_names_recursive(body2, model_names)
+            except AttributeError:
+                pass
+
+    possible_names = copy(hvdconfig.possible_model_names_keras)
+    body = get_body_nodes(root_node)[0]
+    find_wrappers_recursive(body, possible_names)
+    return possible_names
+
+def find_model_names(root_node, possible_names):
+
+    def find_model_names_recursive(body, model_names):
+        # possible_names = copy(hvdconfig.possible_model_names_keras)
+        for elem in body:
+            try:
+                if classname(elem) == 'assign':
+                    try:
+                        if elem.value.func.id in possible_names:
+                            model_names.append(elem.targets[0].id)
+                    except AttributeError:
+                        if elem.value.func.attr in possible_names:
+                            model_names.append(elem.targets[0].id)
+                else:
+                    for body2 in get_body_nodes(elem):
+                        find_model_names_recursive(body2, model_names)
+            except AttributeError:
+                pass
+
+    model_names = list()
+    body = get_body_nodes(root_node)[0]
+    find_model_names_recursive(body, model_names)
+    return model_names
